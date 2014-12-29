049486e41160ec73c851d9fa69c911681d16c1a3
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2014-12-29 19:35:58 +0300

    Bump chef version

--- roles/solo-env-dev.rb
+++ roles/solo-env-dev.rb
@@ -1,8 +1,7 @@
 name "solo-env-dev"
 description "Chef-solo environment for local.dev"
 override_attributes("omnibus_updater" => {
-                      "version" => '11.16.4-1',
-                      "remove_chef_system_gem" => "yes",
-                      "direct_url" => "https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chef-11.16.4-1.el6.x86_64.rpm"
+                      "version" => '12.0.3-1',
+                      "remove_chef_system_gem" => "yes"
                     }
                     )
