c9e58733bad85959cac84f3aeb985f593fd44018
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2015-01-22 14:43:40 +0300

    At vagrant up update Chef and don't touch ntp

--- roles/solo-vmbase.rb
+++ roles/solo-vmbase.rb
@@ -1,7 +1,7 @@
 name "solo-vmbase"
 description "Basic chef-solo role for virtual boxes"
-run_list( "recipe[ssh-keys]",
-          "recipe[ntp]",
+run_list( "recipe[omnibus_updater]",
+          "recipe[ssh-keys]",
           "recipe[kludges-tanabata::chef-solovm]"
           )
 override_attributes(
@@ -11,5 +11,8 @@ override_attributes(
                     "chef-solo" => {
                       "solourl" => "/vagrant/pkg",
                       "tarball" => "tanabata-cuisine.tar.gz"
+                    },
+                    "omnibus_updater" => {
+                      "kill_chef_on_upgrade" => false
                     }
                     )
